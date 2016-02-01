33bfa6d933c201981a5689b9499729101b6d5b2a
arshdeep <arshsingh_sc@yahoo.com>
2016-01-31 18:44:40 -0800

    changed a file name and also read in that file in the python code.

--- assign4.py
+++ assign4.py
@@ -8,6 +8,37 @@ except:
 
 
 
+
+#This function reads in the list of urls that are provided.
+#It then adds those URLs to a Dictionary as keys with empty arrays as values.
+#Returns a dictionary {URL: [list of links]}
+def makeDict():
+    file = open("urls.txt", "r")
+    myDict = {}
+    for aline in file:
+        myDict[aline] = []
+    return myDict
+
+
+
+#The main function
+def mymain():
+    urlDict = makeDict()
+    print("in main")
+    print(urlDict)
+
+
+
+#Find the main function
+if __name__ == '__main__':
+    mymain()
+
+
+
+
+
+
+'''
 sites = 'google yahoo cnn msn'.split()
 
 pat = re.compile(r'<title>+.*</title>+', re.I|re.M)
@@ -21,3 +52,4 @@ for s in sites:
     text = u.read()
     title = re.findall(pat, str(text))
     print(title[0])
+'''
